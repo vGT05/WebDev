52501f85edd600ee8316d1ed61e47d9c2460cc90
vGT05 <roserianriva090@gmail.com>
2026-05-08 20:50:07 -0300

    projeto1

--- Desenvolvimento WEB/Aula 02/Projet Web 01/Projet Web 01/obj/Debug/net10.0/Projet Web 01.AssemblyInfo.cs
+++ Desenvolvimento WEB/Aula 02/Projet Web 01/Projet Web 01/obj/Debug/net10.0/Projet Web 01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_Web_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d67fcf078c5810572d0e65c64d15f1646f4154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd6a0333ae54c9c346c1f75cf6a6095807b5f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_Web_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_Web_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
